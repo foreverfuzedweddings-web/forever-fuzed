4334e4d9cc3cf96034645148c895a93aa41c7c83
foreverfuzedweddings-web <foreverfuzedweddings@gmail.com>
2026-06-08 23:30:58 +0530

    Update data.ts

--- src/app/blogs/data.ts
+++ src/app/blogs/data.ts
@@ -459,3 +459,4 @@ export const blogs: BlogPost[] = [
   nextSlug: "benefits-of-premium-wedding-management-services",
   nextTitle: "Benefits of Premium Wedding Management Services for Signature Experiences",
 }
+ ];
