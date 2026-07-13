706bda5af8768abf36b82a4e8bb39c36f2f30322
Ramesh Chandra <niceramesh82@gmail.com>
2026-07-13 16:47:47 +0530

    Add Google Tag Manager script and noscript to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Cormorant_Garamond, Lato } from "next/font/google";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
@@ -82,6 +83,21 @@ export default function RootLayout({
       className={`${cormorant.variable} ${lato.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+          <Script id="gtm-script" strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N8PVR9ZL');`}
+          </Script>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-N8PVR9ZL"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
           <script
             type="application/ld+json"
             dangerouslySetInnerHTML={{
